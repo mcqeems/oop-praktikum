d7896a975b2c222778ff54c785339685afb71861
mcqeems <mcqeemsofficial@gmail.com>
2025-05-18 12:59:20 +0700

    updated

--- oop.py
+++ oop.py
@@ -1,10 +1,13 @@
 class Mahasiswa:
-    def __init__(self, nama = 0, nim = 0, alamat = 0):
+    def __init__(self, nama: str, nim: str, ipk = 0):
         self.nama = nama
         self.nim = nim
-        self.alamat = alamat
+        self.ipk = ipk
     def show(self):
-        print(f"Nama: {self.nama}, NIM: {self.nim}, Alamat: {self.alamat}")
+        print(f"Nama: {self.nama}, NIM: {self.nim}, IPK: {self.ipk}")
 
-mahasiswa1 = Mahasiswa('Joko', '20242025')
+mahasiswa1 = Mahasiswa('Joko', 20242025, 4.0)
+mahasiswa2 = Mahasiswa('Anton', 20242026)
+
 mahasiswa1.show()
+mahasiswa2.show()
